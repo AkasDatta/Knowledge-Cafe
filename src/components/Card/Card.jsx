af01899ad488fa4bd8552d1e7363c0aff87415d5
Akas_Datta <012akasdatta@gmail.com>
2023-03-31 01:00:56 +0600

    logo picture change json file created and hr line created

---
+++ src/components/Card/Card.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Card.css';
+
+const Card = () => {
+    return (
+        <div className='card-container'>
+            <hr />
+            <div className='products-container'>
+                <h2>jdijifj</h2>
+            </div>
+            <div className='cart-container'>
+                <button>Spent time on road: min</button>
+            </div>
+        </div>
+    );
+};
+
+export default Card;
